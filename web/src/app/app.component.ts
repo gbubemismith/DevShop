edc467063b8cae22fbef7cf33505e98bed300aa3
gbubemismith <gsmithwalter@gmail.com>
2021-06-29 23:19:55 +0100

    Added product interface

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'web';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
